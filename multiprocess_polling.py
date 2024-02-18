5874dc2ed6acbba970fe0c9713ce16b07cc7fa2f
ezhang7423 <ezhang7423@student.palomar.edu>
2024-02-18 06:02:38 -0800

    add polling

--- multiprocess_polling.py
+++ multiprocess_polling.py
@@ -1,14 +1,24 @@
 import os
 import multiprocessing
+import time
 from vllm import LLM, SamplingParams
 
 NUM_GPUS = 4
 
 
+prompts = [
+    f"Write me a story about why {i} is your favourite number.\n\n{i} is my favourite number because "
+    for i in range(100)
+]
+sampling_params = SamplingParams(temperature=0.0, max_tokens=5)
+model_name = "mistralai/Mistral-7B-v0.2"
+
 def run_inference_one_gpu(gpu_id, prompt_list, model_name, sampling_params):
     os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
     llm = LLM(model=model_name)
-    return llm.generate(prompt_list, sampling_params)
+    while True:        
+        llm.generate(prompt_list, sampling_params)
+        time.sleep(.001)
 
 
 # Splits a list into roughly equally sized pieces
@@ -30,11 +40,4 @@ def run_inference_multi_gpu(model_name, prompts, sampling_params):
     return outputs
 
 
-prompts = [
-    f"Write me a story about why {i} is your favourite number.\n\n{i} is my favourite number because "
-    for i in range(100)
-]
-sampling_params = SamplingParams(temperature=0.0, max_tokens=256)
-model_name = "mistralai/Mistral-7B-v0.2"
 outputs = run_inference_multi_gpu(model_name, prompts, sampling_params)
-print(outputs)
