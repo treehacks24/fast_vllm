8f10107d054fb75ff7e440a7065a1d1938ec337d
ezhang7423 <ezhang7423@student.palomar.edu>
2024-02-18 06:55:18 -0800

    calling backend

--- multiprocess_polling.py
+++ multiprocess_polling.py
@@ -25,7 +25,7 @@ def run_inference_one_gpu(gpu_id, prompt_list, model_name, sampling_params):
         ).json()
         if len(full_list) != 0:            
             my_list = split_list(full_list, NUM_GPUS)[gpu_id]
-            results = [[k.text for k in o.outputs] for o in llm.generate(my_list, sampling_params)]
+            results = sum([[k.text for k in o.outputs] for o in llm.generate(my_list, sampling_params)], [])
         else:
             results = []
         time.sleep(1)
