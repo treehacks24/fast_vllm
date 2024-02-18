b2c52919a5fa0754a3c528e68620e5603e955f7a
ezhang7423 <ezhang7423@student.palomar.edu>
2024-02-18 06:19:46 -0800

    calling backend

--- multiprocess_polling.py
+++ multiprocess_polling.py
@@ -1,6 +1,7 @@
 import os
 import multiprocessing
 import time
+import requests
 from vllm import LLM, SamplingParams
 
 NUM_GPUS = 4
@@ -16,14 +17,19 @@ model_name = "mistralai/Mistral-7b-instruct-v0.2"
 def run_inference_one_gpu(gpu_id, prompt_list, model_name, sampling_params):
     os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
     llm = LLM(model=model_name)
-    while True:        
-        llm.generate(prompt_list, sampling_params)
-        time.sleep(.001)
+    results = []
+    while True:       
+        full_list = requests.request('GET', 'http://34.31.37.216/llminference', params={'results': results}).json()
+        my_list = split_list_i(full_list, NUM_GPUS, gpu_id)
+        results = llm.generate(prompt_list, sampling_params)
+        time.sleep(.01)
 
 
 # Splits a list into roughly equally sized pieces
 # split_list(["a", "b", "c", "d", "e", "f", "g"], 3) -> [['a', 'b'], ['c', 'd'], ['e', 'f', 'g']]
 split_list = lambda l, n: [l[i * len(l) // n : (i + 1) * len(l) // n] for i in range(n)]
+
+split_list_i = lambda l, n, i: [l[i * len(l) // n : (i + 1) * len(l) // n] for i in range(n)][i]
 
 
 def run_inference_multi_gpu(model_name, prompts, sampling_params):
